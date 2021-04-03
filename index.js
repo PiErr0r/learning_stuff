9dbca0ffda314d79695e1075d2102ab958091553
Piero <piero@prehnit.hr>
2021-04-03 22:11:56 +0200

    + device io (just o)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const readline = require('readline');
-const createMemory = require('./createMemory');
-const CPU = require('./cpu');
-const instructions = require('./instructions');
 
+const CPU = require('./cpu');
+const createMemory = require('./createMemory');
+const createScreenDevice = require('./screenDevice');
+const instructions = require('./instructions');
+const MemoryMapper = require('./memoryMapper');
+
+const MM = new MemoryMapper();
 const memory = createMemory(256 * 256);
-const cpu = new CPU(memory);
+MM.map(memory, 0x0, 0xffff);
+MM.map(createScreenDevice(), 0x3000, 0x30ff, true);
+const cpu = new CPU(MM);
 const writeableBytes = new Uint8Array(memory.buffer);
 
 const IP = 0;
@@ -21,84 +27,33 @@ const SP = 10;
 const FP = 11;
 
 let i = 0;
-const START = 0x00;
-const MEM1 = 0x01;
-const MEM2 = 0x00
+let boldPos = 5;
+let boldLen = 10;
 
-const MY_SUBROUTINE = 0x3000
+const writeCharToScreen = (char, command, position) => {
 
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x33;
-writeableBytes[i++] = 0x33;
+	writeableBytes[i++] = instructions.MOV_LIT_REG;
+	writeableBytes[i++] = command;
+	writeableBytes[i++] = char.charCodeAt(0);
+	writeableBytes[i++] = R1;
 
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x22;
-writeableBytes[i++] = 0x22;
+	writeableBytes[i++] = instructions.MOV_REG_MEM;
+	writeableBytes[i++] = R1;
+	writeableBytes[i++] = 0x30;
+	writeableBytes[i++] = position;
 
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x11;
-writeableBytes[i++] = 0x11;
+}
 
-writeableBytes[i++] = instructions.MOV_LIT_REG;
-writeableBytes[i++] = 0x12;
-writeableBytes[i++] = 0x34;
-writeableBytes[i++] = R1;
+//"Hello world!".split('').forEach((c, i) => writeCharToScreen(c, i));
 
-writeableBytes[i++] = instructions.MOV_LIT_REG;
-writeableBytes[i++] = 0x56;
-writeableBytes[i++] = 0x78;
-writeableBytes[i++] = R4;
+writeCharToScreen(' ', 0xff, 0);
+for (let i = 0; i <= 0xff; ++i) {
+	const command = i % 2 === 0
+		?	0x01
+		: 0x02;
+	writeCharToScreen('*', command, i);
+}
 
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x00;
-writeableBytes[i++] = 0x00;
+writeableBytes[i++] = instructions.HLT;
 
-writeableBytes[i++] = instructions.CAL_LIT;
-writeableBytes[i++] = (MY_SUBROUTINE & 0xFF00) >> 8;
-writeableBytes[i++] = (MY_SUBROUTINE & 0x00FF);
-
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x44;
-writeableBytes[i++] = 0x44;
-
-i = MY_SUBROUTINE;
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x01;
-writeableBytes[i++] = 0x02;
-
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x03;
-writeableBytes[i++] = 0x04;
-
-writeableBytes[i++] = instructions.PSH_LIT;
-writeableBytes[i++] = 0x05;
-writeableBytes[i++] = 0x06;
-
-writeableBytes[i++] = instructions.MOV_LIT_REG;
-writeableBytes[i++] = 0x07;
-writeableBytes[i++] = 0x08;
-writeableBytes[i++] = R1;
-
-writeableBytes[i++] = instructions.MOV_LIT_REG;
-writeableBytes[i++] = 0x09;
-writeableBytes[i++] = 0x0A;
-writeableBytes[i++] = R8;
-
-writeableBytes[i++] = instructions.RET;
-
-
-cpu.debug();
-cpu.viewMemoryAt(cpu.getRegister('ip'));
-cpu.viewMemoryAt(0xffff - 1 - 42, 44);
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-rl.on('line', () => {
-	cpu.step();
-	cpu.debug();
-	cpu.viewMemoryAt(cpu.getRegister('ip'));
-	cpu.viewMemoryAt(0xffff - 1 - 42, 44);
-})
+cpu.run();
---
+++ screenDevice.js
@@ -0,0 +1,48 @@
+const eraseScreen = () => {
+	process.stdout.write('\x1b[2J');
+}
+
+const moveTo = (x, y) => {
+	process.stdout.write(`\x1b[${y};${x}H`);
+};
+
+const setBold = () => {
+	process.stdout.write('\x1b[1m');	
+}
+
+const setRegular = () => {
+	process.stdout.write('\x1b[0m');	
+}
+
+const createScreenDevice = () => {
+	return {
+		getUint16: () => 0,
+		getUint8: () => 0,
+		setUint16: (address, data) => {
+			const command = (data & 0xff00) >> 8;
+			const characterValue = data & 0x00ff;
+			switch (command) {
+				case 0xff:
+					eraseScreen();
+					break;
+				case 0x01:
+					setBold();
+					break;
+				case 0x02:
+					setRegular();
+					break;
+				default:
+					break;
+			}
+
+			const x = (address % 16) + 1;
+			const y = Math.floor(address / 16) + 1;
+			moveTo(x * 2, y);
+			const char = String.fromCharCode(characterValue);
+			process.stdout.write(char);
+		},
+		setUint8: () => 0,
+	};
+};
+
+module.exports = createScreenDevice;
