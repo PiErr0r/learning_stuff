ae3260c4d00b90c2cb16bd074afc85432bf471df
Piero <piero@prehnit.hr>
2021-11-12 21:24:18 +0100

    * appropriate changes

--- neural_networks/data.js
+++ neural_networks/data.js
@@ -4,17 +4,17 @@ const math = require("./math");
 module.exports = (() => ({
 	spiralData: function(points, classes) {
 		const X = math.zeros(points * classes, 2);
-		const Y = math.zeros(points * classes);
-		console.log(X.dim())
+		const Y = math.zeros(points * classes, 1);
+
 		for (let i = 0; i < classes; ++ i) {
 			const ix = math.range(points * i, points * (i + 1));
 			const r = math.linspace(0, 1, points);
-			const t = math.linspace(i * 4, (i + 1) * 4, points).add(math.randn(points).dot(0.2));
+			const t = math.linspace(i * 4, (i + 1) * 4, points).add(math.randn(points, 1).dot(0.2));
 			for (let j = points * i; j < points * (i + 1); ++j) {
 				const ix = j - points * i;
-				X.d[j][0] = r.d[ix] * Math.sin(t.d[ix] * 2.5);
-				X.d[j][1] = r.d[ix] * Math.cos(t.d[ix] * 2.5);
-				Y.d[j] = i;
+				X[j][0] = r[ix] * Math.sin(t[ix] * 2.5);
+				X[j][1] = r[ix] * Math.cos(t[ix] * 2.5);
+				Y[j] = i;
 			}
 		}
 		return [X, Y];
--- neural_networks/index.js
+++ neural_networks/index.js
@@ -1,49 +1,107 @@
 const { M, V } = require("./math_data");
 const math = require('./math');
-const { Layer } = require("./nn");
+const { Layer, Network } = require("./nn");
 const D = require('./data');
-const fns = require('./activation_fns');
+const fns = require('./activation');
+const { Loss } = require('./loss');
+const { accuracy } = require('./accuracy');
 
-// const X = new M([
-// 	[1, 2, 3, 2.5],
-// 	[2, 5, -1, 2],
-// 	[-1.5, 2.7, 3.3, -0.8]
-// ]);
-
-const [X, Y] = D.spiralData(100, 3);
-
-// console.log('!!',X)
-const layer1 = new Layer(X.d[0].length, 5);
-const layer2 = new Layer(5, 2);
-
-layer1.forward(X);
-// console.log(layer1.output)
-
-console.log(layer1.output.apply(fns.rectLin))
-
-// layer2.forward(layer1.output)
-// console.log(layer2.output)
-
-
-/*
-const weights = new M([
-	[0.2, 0.8, -0.5, 1], 
-	[0.5, -0.91, 0.26, -0.5], 
-	[-0.26, -0.27, 0.17, 0.87]
+const X = new M([
+	[1, 2, 3, 2.5],
+	[2, 5, -1, 2],
+	[-1.5, 2.7, 3.3, -0.8]
 ]);
 
-const biases = new V([2, 3, 0.5]);
-
-const w2 = new M([
-	[0.1, -0.14, .5],
-	[-.5, .12, -.33],
-	[-.44, .73, -.13]
+const Y = new M([
+	[.7, .1, .2],
+	[.1, .5, .4],
+	[.02, .9, .08]
 ]);
 
-const biases2 = new V([-1, 2, -.5]);
+const main = () => {
+	const lds = [
+		{ layer: "Dense", activation: "softMax", inputs: 2, neurons: 3 },
+		{ layer: "Dense", activation: "softMax", inputs: 3, neurons: 3 },
+	];
+	const [X, Y] = D.spiralData(100, 3);
+	const L = new Loss.CategoricalCrossEntropy();
+	for (let i = 0; i<5; ++i) {
+		const NN = new Network(lds);
+		NN.calculate(X);
+		const accy = accuracy(NN.output, Y);
+		console.log("accy: ", (accy * 100).toFixed(4) + "%");
+		console.log("loss: ", L.calculate(NN.output, Y).toFixed(4));
+		// console.log(predictions.slice(290), Y.slice(290))
+	}
+}
 
-const layer_out = inputs.dot(weights.T()).add(biases);
+const main6 = () => {
+	const yt = new V([0, 1, 1]);
+	const yt2 = new M([[1,0,0],[0,1,0],[0,1,0]]);
+	const L = new Loss.CategoricalCrossEntropy();
+	console.log(L.calculate(Y, yt))
+	console.log(L.calculate(Y, yt2))
+}
 
-// console.log(layer_out.dot(w2.T()).add(biases2))
-console.log(math.eye(5))
-*/
+const main5 = () => {
+	const [X, Y] = D.spiralData(100, 3);
+	const layer1 = new Layer.Dense(2, 3);
+	const layer2 = new Layer.Dense(3, 3);
+	layer1.forward(X);
+	layer2.forward(fns.softMax(layer1.output));
+	const out = fns.softMax(layer2.output.slice(0, 5));
+	const L = new Loss.CategoricalCrossEntropy();
+	// console.log(out.dim(), Y.dim(), Y)
+	console.log(L.calculate(out, Y))
+}
+
+const main4 = () => {
+	const out = new M([
+		[4.8, 1.21, 2.385],
+		[8.9, -1.81, 0.2],
+		[1.41, 1.051, 0.026]
+	]);
+	console.log(fns.softMax(out), fns.softMax(out).sum());
+}
+
+const main3 = () => {
+	const [X, Y] = D.spiralData(100, 3);
+
+	// // console.log('!!',X)
+	const layer1 = new Layer.Dense(X[0].length, 5);
+	const layer2 = new Layer.Dense(5, 2);
+
+	layer1.forward(X);
+	// console.log(layer1.output)
+
+
+	layer2.forward(layer1.output.apply(fns.rectLin))
+	console.log(layer2.output)
+}
+
+
+
+const main2 = () => {
+	const weights = new M([
+		[0.2, 0.8, -0.5, 1], 
+		[0.5, -0.91, 0.26, -0.5], 
+		[-0.26, -0.27, 0.17, 0.87]
+	]);
+
+	const biases = new V([2, 3, 0.5]);
+
+	const w2 = new M([
+		[0.1, -0.14, .5],
+		[-.5, .12, -.33],
+		[-.44, .73, -.13]
+	]);
+
+	const biases2 = new V([-1, 2, -.5]);
+
+	const layer_out = inputs.dot(weights.T()).add(biases);
+
+	// console.log(layer_out.dot(w2.T()).add(biases2))
+	console.log(math.eye(5))
+}
+
+main();
