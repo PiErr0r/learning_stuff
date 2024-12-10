27b840e6bdf713926365f651a97d53d4c2a2a71c
piero <piero.anic@gmail.com>
2024-12-10 14:06:08 +0100

    + strings

--- crafting_compilers/compiler/memory.h
+++ crafting_compilers/compiler/memory.h
@@ -2,11 +2,16 @@
 #define clox_memory_h
 
 #include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * count)
 
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : 2 * (capacity))
 #define GROW_ARRAY(type, pointer, oldCount, newCount) (type*)reallocate(pointer, sizeof(type)*(oldCount), sizeof(type)*(newCount))
 #define FREE_ARRAY(type, pointer, oldCount) (type*)reallocate(pointer, sizeof(type)*(oldCount), 0)
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
