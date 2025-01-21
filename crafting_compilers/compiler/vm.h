33e9a63a517bfdc807fe9bfbcf481c01c5d3cfcd
piero <piero.anic@gmail.com>
2025-01-21 14:04:33 +0100

    + closures

--- crafting_compilers/compiler/vm.h
+++ crafting_compilers/compiler/vm.h
@@ -9,7 +9,7 @@
 #define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
-	ObjFunction* function;
+	ObjClosure* closure;
 	uint8_t* ip;
 	Value* slots;
 } CallFrame;
@@ -17,10 +17,12 @@ typedef struct {
 typedef struct {
 	CallFrame frames[FRAMES_MAX];
 	int frameCount;
+
 	Value stack[STACK_MAX];
 	Value* stackTop;
 	Table globals;
 	Table strings;
+	ObjUpvalue* openUpvalues;
 	Obj* objects;
 } VM;
 
