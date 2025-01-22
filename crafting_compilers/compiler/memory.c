caeb237973325706f7efb87db04f01089df48b8f
piero <piero.anic@gmail.com>
2025-01-22 17:51:19 +0100

    + classes and instances

--- crafting_compilers/compiler/memory.c
+++ crafting_compilers/compiler/memory.c
@@ -3,6 +3,7 @@
 #include "compiler.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -70,6 +71,10 @@ static void freeObject(Obj* object) {
 	printf("%p free type %d\n", (void*)object, object->type);
 #endif
 	switch (object->type) {
+		case OBJ_CLASS: {
+			FREE(ObjClass, object);
+			break;
+		}
 		case OBJ_CLOSURE: {
 			ObjClosure* closure = (ObjClosure*)object;
 			FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
@@ -80,6 +85,12 @@ static void freeObject(Obj* object) {
 			ObjFunction* function = (ObjFunction*)object;
 			freeChunk(&function->chunk);	
 			FREE(ObjFunction, object);
+			break;
+		}
+		case OBJ_INSTANCE: {
+			ObjInstance* instance = (ObjInstance*)object;
+			freeTable(&instance->fields);
+			FREE(ObjInstance, object);
 			break;
 		}
 		case OBJ_NATIVE: {
@@ -134,6 +145,11 @@ static void blackenObject(Obj* object) {
 #endif
 
 	switch (object->type) {
+		case OBJ_CLASS: {
+			ObjClass* klass = (ObjClass*)object;
+			markObject((Obj*)klass);
+			break;;
+		}
 		case OBJ_CLOSURE: {
 			ObjClosure* closure = (ObjClosure*)object;
 			markObject((Obj*)closure->function);
@@ -146,6 +162,12 @@ static void blackenObject(Obj* object) {
 			ObjFunction* function = (ObjFunction*)object;
 			markObject((Obj*)function->name);
 			markArray(&function->chunk.constants);
+			break;
+		}
+		case OBJ_INSTANCE: {
+			ObjInstance* instance = (ObjInstance*)object;
+			markObject((Obj*)instance->klass);
+			markTable(&instance->fields);
 			break;
 		}
 		case OBJ_UPVALUE:
--- crafting_compilers/compiler/vm.c
+++ crafting_compilers/compiler/vm.c
@@ -1,13 +1,17 @@
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
 #include "object.h"
 #include "memory.h"
+#include "table.h"
+#include "value.h"
 #include "vm.h"
 
 VM vm;
@@ -85,6 +89,11 @@ static bool call(ObjClosure* closure, int argCount) {
 static bool callValue(Value callee, int argCount) {
 	if (IS_OBJ(callee)) {
 		switch (OBJ_TYPE(callee)) {
+			case OBJ_CLASS: {
+				ObjClass* klass = AS_CLASS(callee);
+				vm.stackTop[-argCount - 1] = OBJ_VAL(newInstance(klass));
+				return true;
+			}
 			case OBJ_CLOSURE:
 				return call(AS_CLOSURE(callee), argCount);
 			case OBJ_NATIVE: {
@@ -261,6 +270,37 @@ static InterpretResult run() {
 				*frame->closure->upvalues[slot]->location = peek(0);
 				break;
 			}
+			case OP_GET_PROPERTY: {
+				if (!IS_INSTANCE(peek(0))) {
+					runtimeError("Only instances have properties.");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				ObjInstance* instance = AS_INSTANCE(peek(0));
+				ObjString* name = READ_STRING();
+
+				Value value;
+				if (tableGet(&instance->fields, name, &value)) {
+					pop(); // instance
+					push(value);
+					break;
+				}
+
+				runtimeError("Undefined property '%s'.", name->chars);
+				return INTERPRET_RUNTIME_ERROR;
+			}
+			case OP_SET_PROPERTY: {
+				if (!IS_INSTANCE(peek(1))) {
+					runtimeError("Only instances have properties.");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+
+				ObjInstance* instance = AS_INSTANCE(peek(1));
+				tableSet(&instance->fields, READ_STRING(), peek(0));
+				Value value = pop();
+				pop();
+				push(value);
+				break;
+			}
 			case OP_EQUAL: {
 				Value b = pop();
 				Value a = pop();
@@ -356,6 +396,9 @@ static InterpretResult run() {
 				frame = &vm.frames[vm.frameCount - 1];
 				break;
 			}
+			case OP_CLASS:
+				push(OBJ_VAL(newClass(READ_STRING())));
+				break;
 		}
 	}
 
